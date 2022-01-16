7cb1aaa626cbbf829c5970108b944cb13a490f9e
XinranTang <sherrytangssr@gmail.com>
2022-01-16 13:58:01 -0500

    Finish exercise for lecture 05

--- lectures/05/hands-on/01/Ghost.h
+++ lectures/05/hands-on/01/Ghost.h
@@ -1,15 +1,36 @@
 #include <iostream>
+using namespace std;
 
 class Ghost
 {
+    const char * name_;
 public:
-    Ghost(const char *name = "Ghost") {}
+    Ghost(const char *name = "Ghost"): name_(name) 
+    {
+        cout << "Construct A" << endl;
+    }
+    virtual ~Ghost() {
+        cout << "Destruct A" << endl;
+    }
 
-    void spook() {}
+    Ghost(const Ghost & c): name_(c.name_) 
+    {
+        cout << "Copy A" <<endl;
+    }
+
+    void spook() const {
+        cout << name_ << " BOO!" << endl;
+    }
+
 };
 
 class Rider : public Ghost // Rider inherits from Ghost
 {
 public:
-    Rider() : Ghost("Rider") {}
+    Rider() : Ghost("Rider") {
+        cout << "Construct B" << endl;
+    }
+    ~Rider() {
+        cout << "Destruct B" << endl;
+    }
 };
